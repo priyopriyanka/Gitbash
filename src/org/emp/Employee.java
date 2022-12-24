0a55df4b9b23c98fb84062a1139b61026e63855c
priyopriyanka <priyochitu2315@gmail.com>
2022-12-24 11:04:19 +0530

    i love my self

---
+++ src/org/emp/Employee.java
@@ -0,0 +1,34 @@
+package org.emp;
+
+public class Employee {
+public void greensOmr()
+{
+	System.out.println("Greensomr in :metupaakam");
+}
+private void greensAdayar()
+{
+	System.out.println("Greensadayar in :padmanabha nagar");
+}
+private void greensTambaram()
+{
+	System.out.println("GreensTambaram in :Apparao Colony");
+}
+private void greensVelacherry()
+{
+	System.out.println("GreensVelacherry in :nagendra Nagar");
+}
+private void greensAnnaNagar()
+{
+	System.out.println("GreensAnnanagar in :W block");
+}
+public static void main(String[]args)
+{
+	Employee e =new Employee();
+	e.greensOmr();
+	e.greensAdayar();
+	e.greensTambaram();
+	e.greensVelacherry();
+	e.greensAnnaNagar();
+	
+}
+}
